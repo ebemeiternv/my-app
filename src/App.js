3d633a06a631aaf759ac021630266099f5318975
emv <ebemeitern@gmail.com>
2024-09-12 18:04:59 +0200

    Built app and resolved index.css issue

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import RecipeForm from './components/RecipeForm'; // Import the RecipeForm component
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, React is running!</h1>
+      <RecipeForm />  {/* Render the RecipeForm component here */}
+    </div>
+  );
+}
+
+export default App;
