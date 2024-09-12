ef13c36eb4455af29f99a6b190552bb6df7075cf
Ebe Meitern <emv@ebes-air.home>
2024-09-12 09:55:01 +0200

    Deploy frontend and backend

--- backend/app.py
+++ backend/app.py
@@ -37,5 +37,7 @@ def get_recipes():
     # Return the response back to the frontend
     return jsonify(response.json())
 
+# Start the app using Heroku's assigned port
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Heroku's port or default to 5000
+    app.run(host='0.0.0.0', port=port, debug=False)
