9ae355946555c38caeaf1f82f19f2e3325c10b8f
Ebe Meitern <emv@ebes-air.home>
2024-09-12 13:30:58 +0200

    Cleaned up static folder structure

--- backend/app.py
+++ backend/app.py
@@ -3,16 +3,17 @@ from flask_cors import CORS
 import os
 import requests
 
-app = Flask(__name__, static_folder='static/build')
+# Define the Flask app and set the correct static folder
+app = Flask(__name__, static_folder=os.path.join('static', 'build'))
 
 # Apply CORS to allow all origins
 CORS(app, resources={r"/*": {"origins": "*"}}, methods=["POST", "GET", "OPTIONS"])
 
-# Serve React App
+# Serve the React App (static files)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve(path):
-    if path != "" and os.path.exists(app.static_folder + '/' + path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
         return send_from_directory(app.static_folder, path)
     else:
         return send_from_directory(app.static_folder, 'index.html')
@@ -27,10 +28,12 @@ def get_recipes():
     ingredients = ",".join(data['ingredients'])
     api_key = '6ff9812470314998a8db9f0087cbf3c2'
 
+    # Query Spoonacular API with ingredients
     url = f'https://api.spoonacular.com/recipes/findByIngredients?ingredients={ingredients}&apiKey={api_key}'
     response = requests.get(url)
 
     return jsonify(response.json())
 
+# Start the Flask app
 if __name__ == '__main__':
     app.run(debug=True)
